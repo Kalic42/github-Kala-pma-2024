b7826577d1ec86c2f2a04f2ea3ad76baebc921e9
Kalic42 <radim.kala@tul.cz>
2024-10-02 21:54:55 +0200

    Quest completed

--- MyApp002MyOwnLinearLayout/app/src/main/java/com/example/myapp002myownlinearlayout/MainActivity.kt
+++ MyApp002MyOwnLinearLayout/app/src/main/java/com/example/myapp002myownlinearlayout/MainActivity.kt
@@ -4,53 +4,43 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //enableEdgeToEdge()
-
         setContentView(R.layout.activity_main)
 
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }*/
-
-        val etName = findViewById<EditText>(R.id.etName)
-        val etSurname = findViewById<EditText>(R.id.etSurname)
-        val etPlace = findViewById<EditText>(R.id.etPlace)
-        val etAge = findViewById<EditText>(R.id.etAge)
+        val etNumber1 = findViewById<EditText>(R.id.etNumber1)
+        val etNumber2 = findViewById<EditText>(R.id.etNumber2)
         val tvInformation = findViewById<TextView>(R.id.tvInformation)
-        val bntSend = findViewById<Button>(R.id.btnSend)
+        val btnCalculate = findViewById<Button>(R.id.btnCalculate)
         val btnDelete = findViewById<Button>(R.id.btnDelete)
 
         // Nastavení obsluhy pro tlačítko Odeslat
-        bntSend.setOnClickListener {
-            val name = etName.text.toString()
-            val surname = etSurname.text.toString()
-            val place = etPlace.text.toString()
-            val age = etAge.text.toString()
+        btnCalculate.setOnClickListener {
+            // Převod zadaných hodnot na čísla (pokud jsou platná)
+            val number1 = etNumber1.text.toString().toIntOrNull()
+            val number2 = etNumber2.text.toString().toIntOrNull()
 
-            // Zobrazení textu v TextView
-            val formattedText =
-                "Jmenuji se $name $surname. Je mi $age let a moje bydliště je $place."
-            tvInformation.text = formattedText
+            if (number1 != null && number2 != null) {
+                // Sečtení čísel A (number1) a B (number2)
+                val sum = number1 + number2
+
+                // Zobrazení textu v TextView
+                val formattedText = "Číslo A je $number1, číslo B je $number2 a jejich součet je $sum."
+                tvInformation.text = formattedText
+            } else {
+                // Pokud jedno z čísel není platné, zobrazí chybovou zprávu
+                tvInformation.text = "Zadejte platná čísla pro A (Number1) a B (Number2)."
+            }
         }
 
-        // Nastavení obsluhy pro tlačítko Odeslat
+        // Nastavení obsluhy pro tlačítko Smazat
         btnDelete.setOnClickListener {
-            etName.text.clear()
-            etSurname.text.clear()
-            etPlace.text.clear()
-            etAge.text.clear()
-
+            etNumber1.text.clear()
+            etNumber2.text.clear()
             tvInformation.text = ""
         }
     }
